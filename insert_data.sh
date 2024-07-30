468d5a6899fad8b195e602ff0ac7423bf258c1db
Tamara Contreras <tacs.laboral@gmail.com>
2024-07-30 22:47:04 +0000

    Save progress: 1047.1

--- insert_data.sh
+++ insert_data.sh
@@ -55,7 +55,11 @@ do
 MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
    echo $MAJOR_ID
     # if not found
+if [[ -z $MAJOR_ID ]]
+then
+  # set to null
 
+fi
     # set to null
 
     # insert student
