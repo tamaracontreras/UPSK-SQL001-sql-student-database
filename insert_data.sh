25f46b8ac126244d6767bb4ed92624797a98c0ab
Tamara Contreras <tacs.laboral@gmail.com>
2024-07-30 21:51:06 +0000

    Save progress: 840.1

--- insert_data.sh
+++ insert_data.sh
@@ -23,6 +23,7 @@ fi
   MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 fi
    # get course_id
+   COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
 
   # if not found
 
